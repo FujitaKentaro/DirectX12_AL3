9854c19d0bef32045d95d822dd53da4791a78b9f
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-04-28 13:32:34 +0900

    KAMATA ENGIN更新

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,15 +1,14 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DebugText.h"
 #include "DirectXCommon.h"
+#include "DebugText.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include <DirectXMath.h>
 
 /// <summary>
 /// ゲームシーン
@@ -47,47 +46,8 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
-	//カメラ上方向の角度
-	//float viewAngle = 1.6f;
-
-	//値を表示したい変数
-	uint32_t value_ = 0;
-
-	//スプライト
-	Sprite* sprite_ = nullptr;
-
-	// 3Dモデル
-	Model* model_ = nullptr;
-
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	//サウンドデーターハンドル
-	uint32_t soundDataHandle_ = 0;
-
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
-
-	//ワールドトランスフォーム
-	WorldTransform worldTransform_[100];
-	//ビュープロジェクション
-	ViewProjection viewProjection_;
 
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
-
-  public:
-	//パーツID
-	enum PartId {
-		Root,		//大元
-		Spine,		//脊椎
-		Chest,		//胸
-		Head,		//頭
-		ArmL,		//左腕
-		ArmR,		//右腕
-		Hip,		//尻
-		LegL,		//左足
-		LegR,		//右足
-	};
 };
