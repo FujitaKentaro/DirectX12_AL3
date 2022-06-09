2af58968f4a5c77c9b8721d0ba446a795c821ff2
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-06-09 15:27:45 +0900

    その２

--- Object/Player.cpp
+++ Object/Player.cpp
@@ -11,25 +11,11 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 	// シングルトンインスタンスを取得する
 	input_ = Input::GetInstance();
 	debugText_ = DebugText::GetInstance();
-
-	worldTransform_.Initialize();
-
-
-	   {
-		// XYZの移動量を設定
-		Vector3 move = {2, 2, 2};
-		Vector3 rotatte = {45, 45, 45};
-		Vector3 scale = {2, 2, 2};
-
-		Matrix4 matWorld_= Affin::matUnit();
-
-		matWorld_ = Affin::matWorld(move,rotatte,scale);
-
-		worldTransform_.matWorld_ *= matWorld_;
-
-		worldTransform_.TransferMatrix();
-		}
-
+	
+		worldTransform_.Initialize();
+	SetTrans();
+		
+	
 }
 
 /// <summary>
@@ -44,7 +30,28 @@ void Player::Update(){
 /// 描画
 /// </summary>
 void Player::Draw(ViewProjection viewProjection){
+
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
 
 }
 
+/// <summary>
+/// ランダム位置セット
+/// </summary>
+void Player::SetTrans() {
+
+	{
+		// XYZの移動量を設定
+		Vector3 move = {distT(engine), distT(engine), distT(engine)};
+		Vector3 rotatte = {distR(engine), distR(engine), distR(engine)};
+		Vector3 scale = {1, 1, 1};
+
+		Matrix4 matWorld_ = Affin::matUnit();
+
+		matWorld_ = Affin::matWorld(move, rotatte, scale);
+
+		worldTransform_.matWorld_ *= matWorld_;
+
+		worldTransform_.TransferMatrix();
+	}
+}
