a44d0ad269a59b4723e7120d2dc76405b49a64e8
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-06-16 14:43:58 +0900

    2

--- Object/Player.cpp
+++ Object/Player.cpp
@@ -11,46 +11,7 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 	input_ = Input::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
-	// 親 大元
-	worldTransform_[PartId::kRoot].Initialize();
-
-	// 子 脊椎
-	worldTransform_[PartId::kSpine].Initialize();
-	worldTransform_[PartId::kSpine].translation_ = {0, 4.5f, 0};
-	worldTransform_[PartId::kSpine].parent_ = &worldTransform_[PartId::kRoot];
-
-	// 上半身
-	// 胸
-	worldTransform_[PartId::kChest].Initialize();
-	worldTransform_[PartId::kChest].translation_ = {0, 0, 0};
-	worldTransform_[PartId::kChest].parent_ = &worldTransform_[PartId::kSpine];
-	// 頭
-	worldTransform_[PartId::kHead].Initialize();
-	worldTransform_[PartId::kHead].translation_ = {0, 4.5f, 0};
-	worldTransform_[PartId::kHead].parent_ = &worldTransform_[PartId::kChest];
-	// 左腕
-	worldTransform_[PartId::kArmL].Initialize();
-	worldTransform_[PartId::kArmL].translation_ = {-4.5f, 0, 0};
-	worldTransform_[PartId::kArmL].parent_ = &worldTransform_[PartId::kChest];
-	// 右腕
-	worldTransform_[PartId::kArmR].Initialize();
-	worldTransform_[PartId::kArmR].translation_ = {4.5f, 0, 0};
-	worldTransform_[PartId::kArmR].parent_ = &worldTransform_[PartId::kChest];
-
-	//下半身
-	// 尻
-	worldTransform_[PartId::kHip].Initialize();
-	worldTransform_[PartId::kHip].translation_ = {0, -4.5f, 0};
-	worldTransform_[PartId::kHip].parent_ = &worldTransform_[PartId::kSpine];
-	// 左足
-	worldTransform_[PartId::kLegL].Initialize();
-	worldTransform_[PartId::kLegL].translation_ = {4.5f, -4.5f, 0};
-	worldTransform_[PartId::kLegL].parent_ = &worldTransform_[PartId::kHip];
-
-	// 右足
-	worldTransform_[PartId::kLegR].Initialize();
-	worldTransform_[PartId::kLegR].translation_ = {-4.5f, -4.5f, 0};
-	worldTransform_[PartId::kLegR].parent_ = &worldTransform_[PartId::kHip];
+	worldTransform_.Initialize();
 }
 
 /// <summary>
@@ -63,6 +24,7 @@ void Player::Update() {
 	{
 		//キャラクターの移動ベクトル
 		Vector3 move = {0, 0, 0};
+	
 		// 平行移動
 		{ // X方向
 			if (input_->PushKey(DIK_RIGHT)) {
@@ -77,25 +39,25 @@ void Player::Update() {
 				move.y = -0.1;
 			}
 		}
-		//回転
-		{ //上半身
-			if (input_->PushKey(DIK_U)) {
-				worldTransform_[kChest].rotation_.y += 1;
-			} else if (input_->PushKey(DIK_I)) {
-				worldTransform_[kChest].rotation_.y += -1;
-			}
-			//下半身
-			if (input_->PushKey(DIK_J)) {
-				worldTransform_[kHip].rotation_.y += 1;
-			} else if (input_->PushKey(DIK_K)) {
-				worldTransform_[kHip].rotation_.y += -1;
-			}
-		}
 
-		worldTransform_[0].translation_ += move;
-		for (int i = 0; i < kNumPartId; i++) {
-			PartUpdate(worldTransform_[i]);
-		}
+		//移動限界座標
+		const float kMoveLimitX = 35;
+		const float kMoveLimitY = 19;
+
+		//範囲を超えない処理
+		worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+		worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+		worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+		worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
+		worldTransform_.translation_ += move;
+		//行列更新
+		MatUpdate(worldTransform_);
+
+		debugText_->SetPos(10,10);
+		    debugText_->Printf(
+		      "player : x,%f  y,%f z,%f", worldTransform_.translation_.x,
+		      worldTransform_.translation_.y, worldTransform_.translation_.z);
 	}
 
 #pragma endregion
@@ -105,25 +67,24 @@ void Player::Update() {
 /// 描画
 /// </summary>
 void Player::Draw(ViewProjection viewProjection) {
-	for (int i = 0; i < kNumPartId; i++) {
-		if (i < 2) {
-			continue;
-		}
-		model_->Draw(worldTransform_[i], viewProjection, textureHandle_);
-	}
+	
+		model_->Draw(worldTransform_, viewProjection, textureHandle_);
+	
 }
 
-void Player::PartUpdate(WorldTransform& worldTransform_) {
+void Player::MatUpdate(WorldTransform& worldTransform_) {
 
-	// 子の更新
+	// パーツの更新
 	
 	// ３移動合成行列を計算
 	worldTransform_.matWorld_ = Affin::matWorld(
 	  worldTransform_.translation_, worldTransform_.rotation_, worldTransform_.scale_);
+	
 	// 親の行列を掛け算代入
 	if (worldTransform_.parent_ != nullptr) {
 		worldTransform_.matWorld_ *= worldTransform_.parent_->matWorld_;
 	}
+	
 	// 行列の転送
 	worldTransform_.TransferMatrix();
 }
